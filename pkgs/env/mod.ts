54164fcafc3b93b686d03b0b54c91027581e2183
zzztttkkk <ztkisalreadytaken@gmail.com>
2023-09-06 17:11:09 +0800

    [🧱chore] (/): -

--- pkgs/env/mod.ts
+++ pkgs/env/mod.ts
@@ -1,14 +1,18 @@
 import { load } from "std/dotenv/mod.ts";
-import { validate } from "vld/mod.ts";
 import { Reflect } from "reflect/mod.ts";
-import { BaseEnvValue, FilePath } from "./types.ts";
+import { RwLock } from "../sync/mod.ts";
+import { assert } from "std/assert/assert.ts";
+import { vld } from "../internal/mod.ts";
 
-await load();
+Object.entries(await load()).forEach(([k, v]) => {
+	Deno.env.set(k, v);
+});
 
 export interface PropOptions {
 	optional?: boolean;
 	default?: any;
 	aliases?: string[];
+	caseSensitive?: boolean;
 
 	// number
 	allowFloat?: boolean;
@@ -36,9 +40,7 @@ export function prop(opts?: PropOptions): PropertyDecorator {
 		const cls = target.constructor;
 		const designType = Reflect.getMetadata("design:type", target, key);
 		if (![String, Number, Boolean].includes(designType)) {
-			if (!designType || !(designType.prototype instanceof BaseEnvValue)) {
-				throw new Error(`bad design:type for "${cls.name}.${key}"`);
-			}
+			throw new Error(`bad design:type for "${cls.name}.${key}"`);
 		}
 		const infos = reflectInfos.get(cls) || new Map();
 		infos.set(key, { designType, opts: opts || {} });
@@ -47,9 +49,22 @@ export function prop(opts?: PropOptions): PropertyDecorator {
 }
 
 function getRawFromEnv(k: string, info: PropInfo): string | undefined {
-	const names = [k, ...(info.opts.aliases || [])] as string[];
+	let names = [k, ...(info.opts.aliases || [])] as string[];
+	let src: { [k: string]: string };
+	if (!info.opts.caseSensitive) {
+		names = names.map((v) => v.toLowerCase());
+		const lowercaseEnvobj = {} as { [k: string]: string };
+
+		Object.entries(Deno.env.toObject()).forEach(([k, v]) => {
+			lowercaseEnvobj[k.toLowerCase()] = v;
+		});
+		src = lowercaseEnvobj;
+	} else {
+		src = Deno.env.toObject();
+	}
+
 	for (const name of names) {
-		const txt = Deno.env.get(name);
+		const txt = src[name];
 		if (txt != null) {
 			return txt;
 		}
@@ -90,46 +105,78 @@ async function getVal(k: string, info: PropInfo): Promise<any> {
 		case Boolean: {
 			return ["true", "t", "T", "True", "TRUE", "ok", "yes", "1"].includes(raw);
 		}
-		default: {
-			return BaseEnvValue.init(info.designType, raw, info.opts);
-		}
 	}
 }
 
 const insCache = new Map<any, any>();
+const rwLock = new RwLock();
 
-export async function Instance<T>(cls: { new (...args: any[]): T }): Promise<T> {
+async function getIns<T>(cls?: { new (): T }): Promise<T> {
 	let ins = insCache.get(cls);
-	if (ins != null) return ins as T;
+	if (ins != null) {
+		return ins as T;
+	}
 
 	const infos = reflectInfos.get(cls);
 	if (!infos) {
 		throw new Error(`${cls} is not registered`);
 	}
 
-	ins = new cls();
+	ins = new cls!();
 
 	for (const [k, info] of infos) {
 		try {
 			ins[k] = await getVal(k, info);
 		} catch (e) {
-			throw new Error(`[EnvClass ${cls.name}], ${e instanceof Error ? e.message : e.toString()}`);
+			throw new Error(`[EnvClass ${cls!.name}], ${e instanceof Error ? e.message : e.toString()}`);
 		}
 	}
 
-	await validate(ins, { enableDebugMessages: true });
+	const errors = await vld.validate(ins, { enableDebugMessages: true });
+	if (errors && errors.length > 0) {
+		throw new Error(
+			`[EnvClass ${cls!.name} Validate Errors]: {${
+				errors.map((v) => `${v.property}(${Object.keys(v.constraints || {})})`)
+			}}`,
+		);
+	}
 	insCache.set(cls, ins);
 	return ins;
+}
+
+export async function Get<T>(cls: { new (): T }): Promise<T> {
+	return await rwLock.withinw(getIns, cls);
 }
 
 if (import.meta.main) {
 	class Config {
 		@prop()
+		@vld.IsIP()
 		host!: string;
 
+		@prop({ aliases: ["ext_cfg"] })
+		@vld.ExistsFilePath()
+		extcfg!: string;
+
 		@prop()
-		x!: FilePath;
+		@vld.Max(100)
+		@vld.Min(20)
+		port!: number;
+
+		@prop()
+		@vld.Match(/^[a-h]{3}$/g)
+		name!: string;
 	}
 
-	console.log(await Instance(Config));
+	const ps = [] as Promise<Config>[];
+	for (let i = 0; i < 100; i++) {
+		ps.push(Get(Config));
+	}
+
+	const inses = await Promise.all(ps);
+	for (const obj of inses) {
+		assert(obj === inses[0]);
+	}
+
+	console.log(inses[0]);
 }
--- pkgs/internal/mod.ts
+++ pkgs/internal/mod.ts
@@ -1,6 +1,8 @@
 import { Stack } from "./stack.ts";
 import { OnExit } from "./death.ts";
 import * as fs from "./fs.ts";
+import * as clsvld from "clsvld/mod.ts";
+import * as vldext from "./vld_exts.ts";
 
 export { fs, OnExit, Stack };
 
@@ -9,3 +11,8 @@ export async function sleep(ms: number) {
 		setTimeout(resolve, ms);
 	});
 }
+
+export const vld = {
+	...clsvld,
+	...vldext,
+};
