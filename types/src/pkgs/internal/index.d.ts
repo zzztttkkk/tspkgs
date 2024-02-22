45729910308bb9836953bb6391f0d24d9f0ba950
zzztttkkk <ztkisalreadytaken@gmail.com>
2024-02-22 13:52:36 +0800

    [🧹 Chore] (/): -

--- types/src/pkgs/internal/index.d.ts
+++ types/src/pkgs/internal/index.d.ts
@@ -1,7 +1,5 @@
-/// <reference types="node" resolution-mode="require"/>
 import { Stack } from "./stack.js";
 import { Hole } from "./hole.js";
-import { inspect } from "util";
 import * as __ from "./__/index.js";
 export { Stack, Hole, __ };
 export declare function ismain(meta: ImportMeta): boolean;
@@ -9,14 +7,3 @@ export declare function source(meta: ImportMeta): string;
 export declare function sourcedir(meta: ImportMeta): string;
 export declare function sleep(ms: number): Promise<void>;
 export declare function UniqueId(v: object): BigInt;
-export declare class TraceObject {
-    #private;
-    constructor(name: string);
-    [inspect.custom](): string;
-    static readonly A: TraceObject;
-    static readonly B: TraceObject;
-    static readonly C: TraceObject;
-    static readonly D: TraceObject;
-    static readonly E: TraceObject;
-    static readonly F: TraceObject;
-}
